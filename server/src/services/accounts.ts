de58a8b99c0ef4846e07922cf372f2151b73ac8e
Asterki <asterki.dev@proton.me>
2024-11-12 14:36:20 -0600

    Feat: Delete user and create recovery code

--- server/src/services/accounts.ts
+++ server/src/services/accounts.ts
@@ -72,49 +72,94 @@ class AccountService {
 		}
 	}
 
+	public async generateRecoveryCode(email: string) {
+		try {
+			const user = await prisma.user.findFirst({
+				where: {
+					email,
+				},
+			})
+			if (!user) return { status: 'user-not-found' }
+
+			const resetToken = uuidv4()
+			await prisma.security.update({
+				where: {
+					userId: user.id,
+				},
+				data: {
+					resetToken,
+				}
+			})
+
+			return {
+				status: 'success',
+				recoveryCode: resetToken,
+			}
+		} catch (error) {
+			Logger.error((error as Error).message, true)
+			return {
+				status: 'internal-error',
+			}
+		}
+	}
+
 	public async deleteUser(
 		userID: string,
 		password: string,
 		tfaCode?: string
 	) {
-		// try {
-		// 	const user: HydratedDocument<User> | null = await UserModel.findOne(
-		// 		{ userID }
-		// 	)
-		// 	if (!user) return { status: 'internal-error' }
-		// 	// Check passwords and TFA CODE
-		// 	if (
-		// 		!bcrypt.compareSync(
-		// 			password,
-		// 			user.preferences.security.password
-		// 		)
-		// 	)
-		// 		return { status: 'invalid-password' }
-		// 	if (user.preferences.security.twoFactor.active) {
-		// 		if (
-		// 			tfaCode &&
-		// 			!speakeasy.totp.verify({
-		// 				secret: user.preferences.security.twoFactor
-		// 					.secret as string,
-		// 				encoding: 'base32',
-		// 				token: tfaCode,
-		// 			})
-		// 		)
-		// 			return { status: 'invalid-tfa' }
-		// 	}
-		// 	// Delete the user and their related documents
-		// 	await UserModel.deleteOne({
-		// 		userID,
-		// 	})
-		// 	return {
-		// 		status: 'success',
-		// 	}
-		// } catch (error) {
-		// 	Logger.error((error as Error).message, true)
-		// 	return {
-		// 		status: 'internal-error',
-		// 	}
-		// }
+		try {
+			const user = await prisma.user.findFirst({
+				where: {
+					id: userID,
+				},
+				include: {
+					preferences: true,
+					security: true,
+				},
+			})
+			if (!user) return { status: 'internal-error' }
+			// Check passwords and TFA CODE
+
+			if (
+				!bcrypt.compareSync(
+					password,
+					user.passwordHash
+				)
+			)
+				return { status: 'invalid-password' }
+
+
+			if (user.security?.twoFactorEnabled) {
+				if (
+					tfaCode &&
+					!speakeasy.totp.verify({
+						secret: user.security.twoFactorSecret as string,
+						encoding: 'base32',
+						token: tfaCode,
+					})
+				)
+					return { status: 'invalid-tfa' }
+			}
+			// Delete the user and their related documents
+			await prisma.user.delete({
+				where: {
+					id: userID,
+				},
+				include: {
+					security: true,
+					preferences: true,
+				}
+			})
+			return {
+				status: 'success',
+			}
+		} catch (error) {
+			Logger.error((error as Error).message, true)
+			return {
+				status: 'internal-error',
+			}
+		}
 	}
 }
 
