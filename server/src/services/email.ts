56ac70fe83cd0ea863b5dfffba944f6a3c8b1efb
Asterki <asterki.dev@proton.me>
2024-11-12 18:50:07 -0600

    Feat: Email multilanguage support

--- server/src/services/email.ts
+++ server/src/services/email.ts
@@ -1,75 +1,98 @@
-import nodeMailer from 'nodemailer';
-import fsExtra from 'fs-extra';
-import path from 'path';
+import nodeMailer from 'nodemailer'
+import fsExtra from 'fs-extra'
+import path from 'path'
 
-import Logger from '../utils/logger';
+import Logger from '../utils/logger'
 
 class MailerService {
-  private static instance: MailerService;
-  private transporter: nodeMailer.Transporter;
+	private static instance: MailerService
+	private transporter: nodeMailer.Transporter
 
-  private emailTemplates = {};
+	private emailTemplates = {}
 
-  private constructor() {
-    this.transporter = nodeMailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: parseInt(process.env.EMAIL_PORT as string),
-      secure: process.env.EMAIL_SECURE === 'true',
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+	private constructor() {
+		this.transporter = nodeMailer.createTransport({
+			host: process.env.EMAIL_HOST,
+			port: parseInt(process.env.EMAIL_PORT as string),
+			secure: process.env.EMAIL_SECURE === 'true',
+			auth: {
+				user: process.env.EMAIL_USER,
+				pass: process.env.EMAIL_PASS,
+			},
+		})
 
-    this.loadEmailTemplates();
-  }
+		this.loadEmailTemplates()
+	}
 
-  private async loadEmailTemplates() {
-    const templateDir = path.join(__dirname, '../emails');
+	private async loadEmailTemplates() {
+		const templateDir = path.join(__dirname, '../emails')
 
-    const files = await fsExtra.readdir(templateDir);
-    for (const file of files) {
-      const template = await fsExtra.readFile(`${templateDir}/${file}`, 'utf-8');
-      // @ts-ignore We're creating the keys and not necessarily expect them to be there
-      this.emailTemplates[file.replace('.html', '') as string] = template;
-    }
+		const languageFolders = await fsExtra.readdir(templateDir)
+		for (const lang of languageFolders) {
+			const langDir = path.join(templateDir, lang)
 
-    Logger.info('Email templates loaded');
-  }
+			// @ts-ignore We're creating the keys and not necessarily expect them to be there
+			this.emailTemplates[lang] = {}
 
-  public static getInstance() {
-    if (!this.instance) this.instance = new MailerService();
-    return this.instance;
-  }
+			const filesInLangFolder = await fsExtra.readdir(
+				path.join(templateDir, lang)
+			)
+			for (const file of filesInLangFolder) {
+				const templateName = file.split('.')[0]
+				const templatePath = path.join(langDir, file)
+				const templateContent = await fsExtra.readFile(
+					templatePath,
+					'utf-8'
+				)
 
-  public async sendMail(to: string, subject: string, html: string) {
-    return this.transporter.sendMail({
-      from: process.env.EMAIL_FROM,
-      to,
-      subject,
-      html,
-    });
-  }
+				// @ts-ignore We're creating the keys and not necessarily expect them to be there
+				this.emailTemplates[lang][templateName] = templateContent
+			}
+		}
 
-  public async getEmailHTMLTemplate(
-    template: string,
-    data: {
-      [key: string]: string;
-    },
-  ) {
-    // @ts-ignore We're creating the keys and not necessarily expect them to be there
-    let emailTemplate = this.emailTemplates[template];
-    if (!emailTemplate) {
-      Logger.error(`Email template ${template} not found`);
-      return '';
-    }
+		Logger.info('Email templates loaded')
+	}
 
-    for (const key in data) {
-      emailTemplate = emailTemplate.replace(`{{${key}}}`, data[key]);
-    }
+	public static getInstance() {
+		if (!this.instance) this.instance = new MailerService()
+		return this.instance
+	}
 
-    return emailTemplate;
-  }
+	public async sendMail(
+		to: string,
+		subject: string,
+		lang: string,
+		html: string
+	) {
+		return this.transporter.sendMail({
+			from: process.env.EMAIL_FROM,
+			to,
+			subject,
+			html,
+		})
+	}
+
+	public async getEmailHTMLTemplate(
+		template: string,
+		lang: string,
+		data: {
+			[key: string]: string
+		}
+	) {
+		// @ts-ignore We're creating the keys and not necessarily expect them to be there
+		let emailTemplate = this.emailTemplates[lang][template]
+
+		if (!emailTemplate) {
+			Logger.error(`Email template not found: ${template}`)
+			return null
+		}
+
+		for (const key in data) {
+			emailTemplate = emailTemplate.replace(`{{${key}}}`, data[key])
+		}
+
+		return emailTemplate
+	}
 }
 
-export default MailerService.getInstance();
+export default MailerService.getInstance()
