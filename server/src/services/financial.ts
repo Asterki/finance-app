568021080d52596cc9b27092baf5fdd7db7d8b0f
Asterki <asterki.dev@proton.me>
2024-11-13 17:04:05 -0600

    Feat: Create transaction financial method

--- server/src/services/financial.ts
+++ server/src/services/financial.ts
@@ -17,26 +17,60 @@ class FinancialService {
 		return FinancialService.instance
 	}
 
-    // Transactions
-	public async createTransaction(userID: string, amount:number, category: string, description: string, date: Date, tags: string[]) {}
+	// Transactions
+	public async createTransaction(
+		userID: string,
+		type: 'expense' | 'income',
+		amount: number,
+		category: string,
+		description: string,
+		date: Date,
+		tags: string[]
+	) {
+		try {
+			const transaction = await prisma.transaction.create({
+				data: {
+					userId: userID,
+					type: type,
+					amount: amount,
+					category: category,
+					description: description,
+					date: date,
+					tags: tags,
+				},
+			})
+			return transaction
+		} catch (error) {
+			Logger.error(error as string)
+			return null
+		}
+	}
 	public async deleteTransaction(transactionID: string) {}
-    public async getTransactions(userID: string, amount?: number) {}
-    public async getTransaction(transactionID: string) {}
-    public async updateTransaction(transactionID: string, userID: string, amount:number, category: string, description: string, date: Date, tags: string[]) {}
+	public async getTransactions(userID: string, amount?: number) {}
+	public async getTransaction(transactionID: string) {}
+	public async updateTransaction(
+		transactionID: string,
+		userID: string,
+		amount: number,
+		category: string,
+		description: string,
+		date: Date,
+		tags: string[]
+	) {}
 
-    // Expenses
+	// Expenses
 	public async createExpense() {}
 	public async deleteExpense() {}
-    public async getExpenses() {}
-    public async getExpense() {}
-    public async updateExpense() {}
+	public async getExpenses() {}
+	public async getExpense() {}
+	public async updateExpense() {}
 
-    // Incomes
+	// Incomes
 	public async createIncome() {}
 	public async deleteIncome() {}
-    public async getIncomes() {}
-    public async getIncome() {}
-    public async updateIncome() {}
+	public async getIncomes() {}
+	public async getIncome() {}
+	public async updateIncome() {}
 }
 
 export default FinancialService.getInstance()
