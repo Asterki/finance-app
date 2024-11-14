584acf37c32b7a4f1027c65b97d2078e305e6cdb
Asterki <asterki.dev@proton.me>
2024-11-14 12:03:00 -0600

    Feat: Financial API service

--- client/src/features/financial/services/financialApi.ts
+++ client/src/features/financial/services/financialApi.ts
@@ -1,109 +1,136 @@
 import axios from 'axios'
-import handleResponseError from '../../../utils/handleResponseError'
 
+import { Transaction } from '../../../../../shared/models'
 import * as Types from '../../../../../shared/api/financial'
 
 const apiEndpoint = `${import.meta.env.VITE_SERVER_HOST}/api/financial`
 
-const fetchUser = async () => {
-	try {
-		const res = await axios.get<Types.FetchResponseData>(`${apiEndpoint}/me`, {
+const getBalance = async (): Promise<number> => {
+	const response = await axios.get<Types.GetBalanceResponseData>(
+		`${apiEndpoint}/get-balance`,
+		{
 			withCredentials: true,
-		})
+		}
+	)
 
-		return {
-			status: res.data.status,
-			user: res.data.status === 'success' ? res.data.user : null,
-		}
-	} catch (error) {
-		if (axios.isAxiosError(error)) {
-			return {
-				status: handleResponseError(error),
-				user: null,
-			}
-		} else {
-			return {
-				status: 'unknown-error',
-				user: null,
-			}
-		}
-	}
+	return response.data.balance
 }
 
-const logout = async () => {
-	try {
-		const response = await axios.post<Types.LogoutResponseData>(
-			`${apiEndpoint}/logout`,
-			{},
-			{
-				withCredentials: true,
-			}
-		)
-		return response.data.status == 'success'
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	} catch (err) {
-		return false
-	}
-}
-
-const login = async (email: string, password: string, tfaCode: string = '') => {
-	try {
-		const res = await axios.post<Types.LoginResponseData>(
-			`${apiEndpoint}/login`,
-			{
-				email,
-				password,
-				tfaCode,
-			} as Types.LoginRequestBody,
-			{
-				withCredentials: true,
-			}
-		)
-		return res.data
-	} catch (error) {
-		if (axios.isAxiosError(error)) {
-			return {
-				status: handleResponseError(error),
-			}
-		} else {
-			return {
-				status: 'unknown-error',
-			}
+const createTransaction = async (
+	date: string,
+	type: 'income' | 'expense',
+	amount: number,
+	category: string,
+	description: string,
+	tags: string[]
+): Promise<{
+	status: 'success'
+	transaction: Transaction
+}> => {
+	const response = await axios.post<Types.CreateTransactionResponseData>(
+		`${apiEndpoint}/createTransaction`,
+		{
+			date,
+			type,
+			amount,
+			category,
+			description,
+			tags,
+		},
+		{
+			withCredentials: true,
 		}
-	}
+	)
+
+	return response.data
 }
 
-const register = async (
-	name: string,
-	email: string,
-	password: string,
-	currency: string,
-	language: string,
-	timezone: string
-) => {
-	try {
-		const res = await axios.post<Types.RegisterResponseData>(
-			`${apiEndpoint}/register`,
-			{
-				name,
-				email,
-				password,
-				currency,
-				language,
-				timezone,
-			} as Types.RegisterRequestBody,
-			{
-				withCredentials: true,
-			}
-		)
-		return res.data.status
-	} catch (error) {
-		if (axios.isAxiosError(error)) {
-			return handleResponseError(error)
-		} else {
-			return 'unknown-error'
+const deleteTransaction = async (transactionID: string): Promise<'success'> => {
+	const response = await axios.post<Types.DeleteTransactionResponseData>(
+		`${apiEndpoint}/delete-transaction`,
+		{
+			transactionID,
+		},
+		{
+			withCredentials: true,
 		}
-	}
+	)
+
+	return response.data.status
 }
 
-export default { fetchUser, logout, login, register }
+const getTransactionsByFilters = async ({
+	count,
+	dayCount,
+	type,
+	category,
+}: Types.GetTransactionsRequestBody): Promise<Transaction[]> => {
+	const response = await axios.post<Types.GetTransactionsResponseData>(
+		`${apiEndpoint}/get-transactions`,
+		{
+			count,
+			dayCount,
+			type,
+			category,
+		},
+		{
+			withCredentials: true,
+		}
+	)
+	return response.data.transactions
+}
+
+const getTransactionsByID = async (
+	transactionID: string
+): Promise<Transaction | 'transaction-not-found'> => {
+	const response = await axios.post<Types.GetTransactionByIDResponseData>(
+		`${apiEndpoint}/get-transaction-by-id`,
+		{
+			transactionID,
+		},
+		{
+			withCredentials: true,
+		}
+	)
+
+	if (response.data.status == 'success') return response.data.transaction
+	return response.data.status
+}
+
+const updateTransaction = async (
+	transactionID: string,
+	amount: number,
+	category: string,
+	description: string,
+	date: string,
+	tags: string[]
+): Promise<{
+	status: 'success'
+	transaction: Transaction
+}> => {
+	const response = await axios.post<Types.UpdateTransactionResponseData>(
+		`${apiEndpoint}/update-transaction`,
+		{
+			transactionID,
+			amount,
+			category,
+			description,
+			date,
+			tags,
+		},
+		{
+			withCredentials: true,
+		}
+	)
+
+	return response.data
+}
+
+export default {
+	getBalance,
+	createTransaction,
+	deleteTransaction,
+	getTransactionsByFilters,
+	getTransactionsByID,
+	updateTransaction,
+}
