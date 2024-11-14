d302446e679d421497615b7cf2dcce0e0a9ee949
Asterki <asterki.dev@proton.me>
2024-11-14 14:39:02 -0600

    Feat: useTransactions

--- client/src/features/transactions/hooks/useTransactions.ts
+++ client/src/features/transactions/hooks/useTransactions.ts
@@ -1,9 +1,47 @@
-import * as React from 'react';
+import * as React from 'react'
+import { isAxiosError } from 'axios'
+import { ResponseError } from '../../../../../shared/models'
 
-import transactionsApi from "../services/transactionsApi"
+import { setTransactions } from '../slices/transactions'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '../../../store'
+
+import transactionsApi from '../services/transactionsApi'
 
 const useFinancial = () => {
-    const fetchTransactions = async () => {}
+	const dispatch = useDispatch()
+	const currentTransactions = useSelector(
+		(state: RootState) => state.transactions.currentTransactions
+	)
+
+	const fetchTransactions = async () => {
+		try {
+			const transactions = await transactionsApi.getTransactionsByFilters(
+				{
+					count: 100,
+				}
+			)
+
+			dispatch(setTransactions(transactions))
+		} catch (error) {
+			if (isAxiosError(error)) {
+				const responseError = error.response?.data as ResponseError
+				return responseError.message
+			} else {
+				// TODO: Handle unknown errors
+				console.log(error)
+				return 'unknown-error'
+			}
+		}
+	}
+
+	React.useEffect(() => {
+		;(async () => {
+			await fetchTransactions()
+		})()
+	})
+
+	return { currentTransactions, fetchTransactions }
 }
 
 export default useFinancial
